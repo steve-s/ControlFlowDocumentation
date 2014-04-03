0221a62188a98e9cbb77b7499ba79fb470660037
Stepan Sindelar <me@stevesindelar.cz>
2014-04-03 09:26:04 +0200

    Some more text in "Static Analysis" section.

--- thesis/compile-mff.bat
+++ thesis/compile-mff.bat
@@ -1,3 +1,11 @@
+
+
+cd chapters
+IncludePygmentize.exe 2.static.tex > 2.static-pyg.tex
+IncludePygmentize.exe 3.existingsw.tex > 2.existingsw-pyg.tex
+IncludePygmentize.exe 4.implementation.tex > 2.implementation-pyg.tex
+cd ..
+
 pdflatex mff-thesis.tex
 bibtex mff-thesis.aux
 pdflatex mff-thesis.tex
--- thesis/compile-mq.bat
+++ thesis/compile-mq.bat
@@ -1,3 +1,12 @@
+
+
+cd chapters
+IncludePygmentize.exe 2.static.tex > 2.static-pyg.tex
+IncludePygmentize.exe 3.existingsw.tex > 2.existingsw-pyg.tex
+IncludePygmentize.exe 4.implementation.tex > 2.implementation-pyg.tex
+cd ..
+
+
 pdflatex mq-report.tex
 bibtex mq-report.aux
 pdflatex mq-report.tex
