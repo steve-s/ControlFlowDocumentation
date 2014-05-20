6be8e750abe5f564bb1d43fa337f17658dd51342
Stepan Sindelar <me@stevesindelar.cz>
2014-05-20 09:52:49 +0200

    Evaluation chapter skeleton...

--- thesis/compile-mff.bat
+++ thesis/compile-mff.bat
@@ -4,6 +4,7 @@ cd chapters
 IncludePygmentize.exe 2.static.tex > 2.static-pyg.tex
 IncludePygmentize.exe 3.existingsw.tex > 3.existingsw-pyg.tex
 IncludePygmentize.exe 4.implementation.tex > 4.implementation-pyg.tex
+IncludePygmentize.exe 5.results.tex > 5.results-pyg.tex
 cd ..
 
 pdflatex mff-thesis.tex
